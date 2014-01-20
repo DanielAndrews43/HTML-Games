afdb3dde32af03a475bbe87b6cffece9ae03da3f
Daniel Andrews <Danielandrewsre@gmail.com>
2014-01-19 19:57:11 -0800

    game features
    Added things like score, winning the game, info text, etc.

--- src/game01.js
+++ src/game01.js
@@ -15,12 +15,22 @@ var logo,
     paddle1,
     paddle2,
     ball,
+    
     PADDLE_SPEED = 3,
-    p1Score = 0,
-    p2Score = 0,
-    WIN_SCORE = 10,
+    WIN_SCORE = 5,
     PLAYER_CONSTANT = 1,
     COMPUTER_CONSTANT = -1,
+    
+    p1Score = 0,
+    p2Score = 0,
+    p1ScoreText,
+    p2ScoreText,
+    
+    startGameText,
+    endGameText,
+    
+    startGame,
+    gameRunning = true,
     currFrame = 0,
     paddles;
 
@@ -29,6 +39,15 @@ function create() {
     "use strict";
     
     game.stage.backgroundColor = '#63d1f1';
+    
+    p1ScoreText = game.add.text(75, game.height - 40, 'Score: 0', { font: "30px Arial", fill: "#ffffff" });
+    p1ScoreText.anchor.setTo(0.5, 0.5);
+    p2ScoreText = game.add.text(game.width - 75, game.height - 40, 'Score: 0', { font: "30px Arial", fill: '#FFFFFF' });
+    p2ScoreText.anchor.setTo(0.5, 0.5);
+    startGameText = game.add.text(game.world.centerX, game.world.centerY, 'PRESS SPACE TO START', { font: "30px Arial", fill: "#FFFFFF" });
+    startGameText.anchor.setTo(0.5, 0.5);
+    endGameText = game.add.text(game.world.centerX, game.world.centerY, '', { font: "30px Arial", fill: "#FFFFFF" });
+    endGameText.anchor.setTo(0.5, 0.5);
  
     paddle1 = game.add.sprite(20, game.world.centerY, 'paddle');
     paddle1.anchor.setTo(0.5, 0.5);
@@ -52,8 +71,9 @@ function create() {
     paddles.add(paddle1);
     paddles.add(paddle2);
     
-    newBall();
+    startGame = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     
+    newBall();
 }
 
 function restart() {
@@ -61,11 +81,19 @@ function restart() {
     ball.kill();
 }
 
-function gameWin(side) {
+function gameOver(side) {
     
     "use strict";
     
-    //Print text about win/lose, resest stats, ask to play again
+    if (side === 1) {
+        endGameText.content = "You Lose!!";
+    } else {
+        endGameText.content = "You Win!";
+    }
+    
+    gameRunning = false;
+    startGameText.visible = false;
+    endGameText.visible = true;
 }
 
 function getVelocity() {
@@ -81,6 +109,9 @@ function getVelocity() {
 
 function newBall() {
     "use strict";
+    
+    startGameText.visible = true;
+    
     ball = game.add.sprite(game.world.centerX, game.world.centerY, 'ball');
     ball.anchor.setTo(0.5, 0.5);
     ball.body.collideWorldBounds = true;
@@ -88,23 +119,27 @@ function newBall() {
     ball.body.velocity.x = getVelocity();
     ball.body.velocity.y = getVelocity();
     ball.name = 'ball';
+    
+    ball.kill();
 }
 
 function hitWall(side) {
     
     "use strict";
     
-    if (side === 0) {
+    if (side === -1) {
         p1Score += 1;
+        p1ScoreText.content = 'Score: ' + p1Score;
     } else {
         p2Score += 1;
+        p2ScoreText.content = 'Score: ' + p2Score;
     }
     
     ball.kill();
     newBall();
     
     if (p1Score === WIN_SCORE || p2Score === WIN_SCORE) {
-        gameWin(side);
+        gameOver(side);
     }
 }
 
@@ -126,18 +161,32 @@ function collisionHandler(ball, paddle) {
 function update() {
     "use strict";
     
+    if (startGame.isDown && gameRunning) {
+        ball.revive();
+        startGameText.visible = false;
+    }
+    
     game.physics.collide(paddles, ball, collisionHandler, null, paddles);
     
     //move paddle to mouse y pos
     paddle1.y = game.input.y;
     
+    //make it move faster the closer the ball is to the paddle
+    //var currSpeed = PADDLE_SPEED + ((ball.body.x / game.width) - 0.5); CAN'T MOVE HALF A PIXEL!
+    var currPos = ball.body.x / game.width,
+        currSpeed = PADDLE_SPEED;
+    if (currPos < 0.25) {
+        currSpeed = PADDLE_SPEED * 0.5;
+    } else if (currPos > 0.75) {
+        currSpeed = PADDLE_SPEED * 1.5;
+    }
+    
     //move computer paddle based on ball's position
-    //Only moves half of the frames
     if (Math.abs(paddle2.y - ball.y) > 50) {
         if (paddle2.y > ball.y) {
-            paddle2.y -= PADDLE_SPEED;
+            paddle2.y -= currSpeed;
         } else {
-            paddle2.y += PADDLE_SPEED;
+            paddle2.y += currSpeed;
         }
     }
     currFrame += 1;
